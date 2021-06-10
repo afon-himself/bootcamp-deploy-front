8dd65e8018489d7baad548cb0047debe1f21407c
Ivan Afonchenko <afon.himself@gmail.com>
2021-06-10 10:36:41 +0300

    add users list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, []);
+
   return (
     <div className="App">
-      
+      {users.map(user => (
+        <div key={user._id}>
+          <p>{user.name}</p>
+          <p>{user.age}</p>
+        </div>
+      ))}
     </div>
   );
 }
